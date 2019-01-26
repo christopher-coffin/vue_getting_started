509709971b580d05f4ae84f57a9b9815ff49ff69
coffinworldviz <coffin@worldviz.com>
2019-01-25 22:00:23 -0800

    vue sample working no typescript

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <BabylonWorld msg="Babylon And Vue World"/>
+  </div>
+</template>
+
+<script>
+import BabylonWorld from './components/BabylonWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    BabylonWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Roboto', sans-serif;;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-weight: 100;
+}
+</style>
